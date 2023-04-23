5c28e7b864d55890ef2336c389e7e4841c1065b7
LilyanLefevre <lilyandu70.7@gmail.com>
2023-04-23 22:49:37 +0200

    DONE Add index method in controllers + automatic hash of password when create a user

--- app/Http/Controllers/RechirpController.php
+++ app/Http/Controllers/RechirpController.php
@@ -4,11 +4,33 @@ namespace App\Http\Controllers;
 
 use App\Models\Rechirp;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class RechirpController extends Controller
 {
     public function __construct()
     {
         $this->authorizeResource(Rechirp::class, 'rechirp');
+    }
+
+    public function index(Request $request): JsonResponse
+    {
+        $validated = $request->validate([
+            'page' => ['nullable', 'integer'],
+            'per_page' => ['nullable', 'integer'],
+            'sort_by' => ['nullable', 'string'],
+            'sort_type' => ['nullable', 'string'],
+        ]);
+
+        $users = Rechirp::query();
+
+        if(array_key_exists('page', $validated)){
+            $users = $users
+                ->orderBy($validated['sort_by'] ?? 'updated_at', $validated['sort_type'] ?? 'asc')
+                ->paginate($validated['per_page'] ?? 25);
+        }else{
+            $users = $users->get();
+        }
+        return response()->json($users);
     }
 }
--- app/Http/Controllers/TrendingTopicController.php
+++ app/Http/Controllers/TrendingTopicController.php
@@ -4,11 +4,33 @@ namespace App\Http\Controllers;
 
 use App\Models\TrendingTopic;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class TrendingTopicController extends Controller
 {
     public function __construct()
     {
         $this->authorizeResource(TrendingTopic::class, 'trending_topic');
+    }
+
+    public function index(Request $request): JsonResponse
+    {
+        $validated = $request->validate([
+            'page' => ['nullable', 'integer'],
+            'per_page' => ['nullable', 'integer'],
+            'sort_by' => ['nullable', 'string'],
+            'sort_type' => ['nullable', 'string'],
+        ]);
+
+        $users = TrendingTopic::query();
+
+        if(array_key_exists('page', $validated)){
+            $users = $users
+                ->orderBy($validated['sort_by'] ?? 'updated_at', $validated['sort_type'] ?? 'asc')
+                ->paginate($validated['per_page'] ?? 25);
+        }else{
+            $users = $users->get();
+        }
+        return response()->json($users);
     }
 }
